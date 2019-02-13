1b900708fd205fc2cbd4b8fd94ab7d5e2e7258fa
Marijn van der Horst <marijn.vander.horst@marketredesign.com>
2019-02-13 15:55:05 +0100

    Fixed controller not having extract file methods.

--- src/Aacotroneo/Saml2/Http/Controllers/Saml2Controller.php
+++ src/Aacotroneo/Saml2/Http/Controllers/Saml2Controller.php
@@ -55,6 +55,32 @@ class Saml2Controller extends Controller
         $this->saml2Auth = new Saml2Auth($auth);
     }
 
+    protected function extractPkeyFromFile($path) {
+        $res = openssl_get_privatekey($path);
+        if (empty($res)) {
+            throw new \Exception('Could not read private key-file at path \'' . $path . '\'');
+        }
+        openssl_pkey_export($res, $pkey);
+        openssl_pkey_free($res);
+        return $this->extractOpensslString($pkey, 'PRIVATE KEY');
+    }
+
+    protected function extractCertFromFile($path) {
+        $res = openssl_x509_read(file_get_contents($path));
+        if (empty($res)) {
+            throw new \Exception('Could not read X509 certificate-file at path \'' . $path . '\'');
+        }
+        openssl_x509_export($res, $cert);
+        openssl_x509_free($res);
+        return $this->extractOpensslString($cert, 'CERTIFICATE');
+    }
+
+    protected function extractOpensslString($keyString, $delimiter) {
+        $keyString = str_replace(["\r", "\n"], "", $keyString);
+        $regex = '/-{5}BEGIN(?:\s|\w)+' . $delimiter . '-{5}\s*(.+?)\s*-{5}END(?:\s|\w)+' . $delimiter . '-{5}/m';
+        preg_match($regex, $keyString, $matches);
+        return empty($matches[1]) ? '' : $matches[1];
+    }
 
     /**
      * Generate local sp metadata
--- src/Aacotroneo/Saml2/Saml2ServiceProvider.php
+++ src/Aacotroneo/Saml2/Saml2ServiceProvider.php
@@ -56,31 +56,4 @@ class Saml2ServiceProvider extends ServiceProvider
     {
         return array();
     }
-
-    protected function extractPkeyFromFile($path) {
-        $res = openssl_get_privatekey($path);
-        if (empty($res)) {
-            throw new \Exception('Could not read private key-file at path \'' . $path . '\'');
-        }
-        openssl_pkey_export($res, $pkey);
-        openssl_pkey_free($res);
-        return $this->extractOpensslString($pkey, 'PRIVATE KEY');
-    }
-
-    protected function extractCertFromFile($path) {
-        $res = openssl_x509_read(file_get_contents($path));
-        if (empty($res)) {
-            throw new \Exception('Could not read X509 certificate-file at path \'' . $path . '\'');
-        }
-        openssl_x509_export($res, $cert);
-        openssl_x509_free($res);
-        return $this->extractOpensslString($cert, 'CERTIFICATE');
-    }
-
-    protected function extractOpensslString($keyString, $delimiter) {
-        $keyString = str_replace(["\r", "\n"], "", $keyString);
-        $regex = '/-{5}BEGIN(?:\s|\w)+' . $delimiter . '-{5}\s*(.+?)\s*-{5}END(?:\s|\w)+' . $delimiter . '-{5}/m';
-        preg_match($regex, $keyString, $matches);
-        return empty($matches[1]) ? '' : $matches[1];
-    }
 }
